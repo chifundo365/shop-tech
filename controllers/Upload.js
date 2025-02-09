c6cd3cd1a8735978a051bb96c936767d2acf9988
chifundo biziweck <chifundobiziweck@gmail.com>
2025-02-09 02:18:48 +0200

    API - V1 Restructuring code

--- controllers/Upload.js
+++
@@ -1,9 +0,0 @@
-import multer from "multer";
-
-// Configure multer to store files in memory as buffers
-const upload = multer({
-  storage: multer.memoryStorage(), // Use memory storage
-  limits: { fileSize: 300 * 1024 * 1024 } // 300MB file size limit
-});
-
-export default upload;
--- routes/login.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import signInController from "../controllers/SignInController.js";
-
-const Router = express.Router();
-
-Router.route("/").post(signInController);
-
-export default Router;
